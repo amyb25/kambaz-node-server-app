7fa2e1f1b68cd2d3b4fdd782cb34fe59727331c8
Amy Baez <baez.am@northeastern.edu>
2025-04-16 20:08:58 -0400

    enrollments dao change

---  Kambaz/Enrollments/dao.js
+++  Kambaz/Enrollments/dao.js
@@ -1,17 +1,17 @@
 import model from "./model.js";
 export async function findCoursesForUser(userId) {
- const enrollments = await model.find({ user: userId }).populate("course");
- return enrollments.map((enrollment) => enrollment.course);
-}
-export async function findUsersForCourse(courseId) {
- const enrollments = await model.find({ course: courseId }).populate("user");
- return enrollments.map((enrollment) => enrollment.user);
-}
-export function enrollUserInCourse(user, course) {
-  const newEnrollment = { user, course, _id: `${user}-${course}` };
-  return model.create(newEnrollment);
- }
- export function unenrollUserFromCourse(user, course) {
-  return model.deleteOne({ user, course });
+  const enrollments = await model.find({ user: userId }).populate("course");
+  return enrollments.filter((e) => e.course !== null).map((enrollment) => ({...enrollment.course._doc, enrolled: true}));
  }
  
+export async function findUsersForCourse(courseId) {
+ const enrollments = await model.find({ course: courseId }).populate("user");
+ return enrollments.filter((e) => e.user !== null).map((enrollment) => enrollment.user);
+}
+export function enrollUserInCourse(user, course) {
+  const newEnrollment = { user, course, _id: (new Date()).getTime()+'' };
+  return model.create(newEnrollment);
+}
+export function unenrollUserFromCourse(user, course) {
+  return model.deleteOne({ user, course });
+}
